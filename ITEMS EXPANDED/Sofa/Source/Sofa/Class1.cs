0efe16a9d20bc670da91023ed9879e370f617ec8
Nikita Pasechny <nikita.pasechny@appvales.com>
2022-12-11 04:04:16 +0200

    update for 11.12.22

---
+++ ITEMS EXPANDED/Sofa/Source/Sofa/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace DoctorStupid.Sofa
+{
+    [StaticConstructorOnStartup]
+    public static class DoctorStupidSofaInitialize
+    {
+        static DoctorStupidSofaInitialize()
+        {
+            ThingDef sofaDef = DefDatabase<ThingDef>.GetNamed("DocStu_Sofa", true);
+            if (LoadedModManager.GetMod<SofaMod>().GetSettings<SofaSettings>().couchMode)
+            {
+                sofaDef.label = "Couch";
+            }
+        }
+    }
+}
---
+++ ITEMS EXPANDED/Sofa/Source/Sofa/Class2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Verse;
+using UnityEngine;
+
+namespace DoctorStupid.Sofa
+{
+    public class SofaSettings : ModSettings
+    {
+        public bool couchMode;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref couchMode, "sofaCouchMode");
+            base.ExposeData();
+        }
+    }
+
+    public class SofaMod : Mod
+    {
+        SofaSettings settings;
+
+        public SofaMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<SofaSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("Couch Mode (REQUIRES RESTART)", ref settings.couchMode, "Replaces all sofas with couches when enabled");
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+        public override string SettingsCategory()
+        {
+            return "Literally just a sofa";
+        }
+    }
+}
